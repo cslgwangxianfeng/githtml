38d4dda858f9bb59e31d10aacd0a1363c2cf2651
cslgwangxianfeng <736161541@qq.com>
2013-10-18 09:59:22 +0800

    web
    放大镜效果的完成

--- 放大镜/js/jQuerydemo.js
+++ 放大镜/js/jQuerydemo.js
@@ -2,17 +2,23 @@ $(function(){
     initEleEvents();	
 });
 
-var x=pageX;
-var y=100;
+
 
 function initEleEvents(){
-	$("#pic").mouseover(function(e){
+	$("#pic").mouseover(function(){
+		$("#pic img").css("display","none");
+	})
+
+    $("#pic").mouseout(function(){
+		$("#pic img").css("display","block");
+	})
+	$("#pic").mousemove(function(e){
 		var x=e.clientX-($(window).width()-200)/2;
         var y=e.clientY;
-        x=x/1000*100%;
-		y=y/600*100%;
-		$("#pic img").css("display","none");
-		$("#pic").css("background-position",x+"% "+y+"%");
+        x=x/200*100;
+		y=y/120*100;
+		
+		$("#pic").css("background-position",x+"%" + y+"%");
 	})
 };
     
