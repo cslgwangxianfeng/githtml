c7acf327c6f49324c609a047290076478185252d
cslgwangxianfeng <736161541@qq.com>
2013-10-22 18:13:16 +0800

    web
    好友页面的js特效

--- 好友页面/js/jQuerydemo.js
+++ 好友页面/js/jQuerydemo.js
@@ -3,4 +3,10 @@ $(function(){
     	
 });
 
-function initEleEvents(){}
+function initEleEvents(){
+
+		$(".itmquxiaogz").click(function(){
+			$(this).parent(".itmtop").parent(".itm").parent(".contentli").hide();
+		});
+
+}
